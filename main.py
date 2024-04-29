0396c87e2fa286120cc06f8a1b23732ca2e743d9
annan1310 <141527879+annan1310@users.noreply.github.com>
2024-04-29 17:14:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,9 +52,9 @@ twitch_miner = TwitchChannelPointsMiner(
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
         ),
         discord=Discord(
-            webhook_api="https://discord.com/api/webhooks/0123456789/0a1B2c3D4e5F6g7H8i9J",  # Discord Webhook URL
+            webhook_api="https://discord.com/api/webhooks/1223651178710175875/GmGsxXHeu6-MT5FMPPXNNrGM7G7J1rkiQwRXQ9JlIdUCKPq4JCQjrYFl6MN9_UOFN1dy",  # Discord Webhook URL
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
-                    Events.BET_LOSE, Events.CHAT_MENTION],                                  # Only these events will be sent to the chat
+                    Events.BET_LOSE, Events.CHAT_MENTION, Events.GAIN_FOR_WATCH, Events.GAIN_FOR_CLAIM],                             # Only these events will be sent to the chat
         ),
         matrix=Matrix(
             username="twitch_miner",                                                   # Matrix username (without homeserver)
